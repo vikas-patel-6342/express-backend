a0aaa62e2d94d879d07d23f2be91ac30f466595e
vikas-patel-6342 <vikaspatel101299@gmail.com>
2025-04-21 17:22:32 +0530

    Fix some bugs and unlink the images from local storage

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,72 +5,72 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  // Steps for Registering New User
-  // 1. Get User Data form Frontend
-  // 2. Not Empty and correct email validation to be applied
-  // 3. Check if user is already exist or not using => {Email and Username}
-  // 4. Files are avilable or not. => (Avatar Image and CoverImage)
-  // 5. Upload Images to the cloudinary.
-  // 6. Check images to be uploaded on clodinary.
-  // 7. User object to be created for uploading the data on MongoDB.
-  // 8. Remove Password and refresh token from the response.
-  // 9. Check for the response and return the response and if error come then send error.
+  //  Steps for Registering New User
 
-  // 1. Get User Data form Frontend
-  const { userName, email, fullName, password } = req.body; //JSON data or form data will be handle with req.body
+  // * 1. Get User Data form Frontend
+  const { userName, email, fullName, password } = req.body; // ! JSON data or form data will be handle with req.body
 
-  // 2. Not Empty and correct email validation to be applied
+  // * 2. Not Empty and correct email validation to be applied
   if (
     [userName, email, fullName, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required !");
   }
 
-  // Email validation
+  // ! Email validation
   const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!pattern.test(email.trim())) {
     throw new ApiError(400, "Please Enter Correct Email Address !");
   }
 
-  // 3. Check if user is already exist or not using => {Email and Username}
+  // * 3. Check if user is already exist or not using => {Email and Username}
 
-  const existingUser = User.findOne({
+  const existingUser = await User.findOne({
     $or: [{ userName }, { email }],
   });
 
   if (existingUser) {
-    throw ApiError(409, "Username and Email Already exist !");
+    throw new ApiError(409, "Username and Email Already exist !");
   }
 
-  // 4. Files are avilable or not. => (Avatar Image and CoverImage)
-  const avatarLocalPath = res.files?.avatar[0]?.path;
-  const coverImageLocalPath = res.files?.coverIamge[0]?.path;
+  // * 4. Files are avilable or not. => (Avatar Image and CoverImage)
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  //  const coverImageLocalPath = req.files?.coverIamge[0]?.path;
 
-  // Check if Avatar Image is available or not
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
+
+  // ! Check if Avatar Image is available or not
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar Image is Not available !");
   }
 
-  // 5. Upload Images to the cloudinary.
+  // * 5. Upload Images to the cloudinary.
   const avatar = await uploadOnCloudinary(avatarLocalPath);
-  const coverIamge = await uploadOnCloudinary(coverImageLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
-  // Check if avatar image is uploaded or not on cloudinary
+  // ! Check if avatar image is uploaded or not on cloudinary
   if (!avatar) {
-    throw new ApiError(400, "There is a problem in uploading Aavatarr Image !");
+    throw new ApiError(400, "There is a problem in uploading Aavatar Image !");
   }
 
-  // 7. User object to be created for uploading the data on MongoDB.
+  // * 7. User object to be created for uploading the data on MongoDB.
   const newUser = await User.create({
     userName: userName.toLowerCase(),
     fullName,
     email,
     password,
     avatar: avatar.url,
-    coverIamge: coverIamge?.url || "Cover Image Not Available",
+    coverImage: coverImage?.url || "",
   });
 
-  // Check if user is created or not
+  // ! Check if user is created or not
   const createdUser = await User.findById(newUser._id).select(
     "-password -refreshToken"
   );
@@ -79,11 +79,12 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Something went wrong while creating a User !");
   }
 
-  // 8. Return the response to the user
-
+  // * 8. Return the response to the user
   return res
-    .status(201)
-    .json(200, createdUser, "New User Created Successfully");
+    .status(200)
+    .json(
+      new ApiResponse(200, "User is Successfully Registered !", createdUser)
+    );
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,4 @@
 import { v2 as cloudinary } from "cloudinary";
-import { log } from "console";
 import fs from "fs";
 
 // Cloudinary Configuration
@@ -15,12 +14,13 @@ const uploadOnCloudinary = async (localFilePath) => {
     if (!localFilePath)
       return console.log("There is problem in uploading file");
 
-    // Upload on Clodinary
+    // * Upload on Clodinary
     const fileResponse = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
 
-    // If file is successfully uploaded on clodinary
+    fs.unlinkSync(localFilePath);
+    // ! If file is successfully uploaded on clodinary
     console.log(
       "File is successfully uploaded on cloudinary",
       fileResponse.url
@@ -28,7 +28,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
     return fileResponse;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // Remove the temporary file from the database / server when upload is failed.
+    fs.unlinkSync(localFilePath); // * Remove the temporary file from the database / server when upload is failed.
 
     return null;
   }
