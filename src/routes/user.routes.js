59f9bacd9f6945ce5836be2e22a2a62b8047290a
vikas-patel-6342 <vikaspatel101299@gmail.com>
2025-05-06 16:39:17 +0530

    Routers Added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   logoutUser,
   refreshAccesssToken,
+  updateUserPassword,
+  getCurrentUser,
+  updateUserDetails,
+  updateAvatarImage,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,30 @@ router.route("/logout").post(verifyJWT, logoutUser);
 
 // * Refresh Token Route
 router.route("/refresh-token").post(refreshAccesssToken);
+
+// * Change Password Route
+router.route("/change-password").post(verifyJWT, updateUserPassword);
+
+// * Get Curretnt User Route ( Using Get Methode because user don't send any data)
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+
+// * Update User Route
+router.route("/update-user").patch(verifyJWT, updateUserDetails); // * By using patch methode only updated the details which user updated.
+
+// * Update Avatar Image Route
+router
+  .route("/update-avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateAvatarImage);
+
+// * Update Cover Image Route
+router
+  .route("/update-cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+// * Get User Channel using Params
+router.route("/channel/:userName").get(verifyJWT, getUserChannelProfile);
+
+// * Get User Channel using Params
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
